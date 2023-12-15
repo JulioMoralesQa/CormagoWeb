dd077071b074800be0371d1e99cda5cc49bde1fa
julio <desarrollofrontendweb@cormago.com>
2023-12-15 17:38:22 -0600

    segundo commit

--- assets/js/plugins/presentation-page/scriptMip.js
+++ assets/js/plugins/presentation-page/scriptMip.js
@@ -1,20 +1,24 @@
-const btn = document.getElementById('button');
+emailjs.init('MGF6Fn3DXYfns3xOj');
 
-document.getElementById('contact-form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  var Telefono=document.getElementById('phone').value;
+  var contactParams = {
+    from_profilePro: document.getElementById('profilePro').value,
+    from_name: document.getElementById('firstName').value, // Corrige aquí el typo de 'firtsName' a 'firstName'
+    from_lastname: document.getElementById('lastName').value,
+    from_company: document.getElementById('company').value,
+    from_position: document.getElementById('position').value,
+    from_email: document.getElementById('email').value,
+    phoneNumber:Telefono,
+  };
 
-   btn.value = 'Sending...';
-
-   const serviceID = 'service_xcol6gm';
-   const templateID = 'template_a7m0cy4';
-
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'sendEmail';
-      alert('Sent!');
-    }, (err) => {
-      btn.value = 'sendEmail';
-      alert(JSON.stringify(err));
+  emailjs.send('service_xcol6gm', 'template_a7m0cy4', contactParams)
+    .then(function(response) {
+      console.log('SUCCESS!', response.status, response.text);
+      alert('Tu mensaje ha sido enviado!');
+    }, function(error) {
+      console.log('FAILED...', error);
+      alert('Falló el envío del mensaje: ' + JSON.stringify(error));
     });
 });
